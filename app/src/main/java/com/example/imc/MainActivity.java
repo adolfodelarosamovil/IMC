4688d86505fcb4650baa0f7d4b56ac454fedde97
Adolfo <adolfodelarosa2012@gmail.com>
2019-10-07 18:58:20 +0200

    Que se conserven los resultados al girar la imagen

--- app/src/main/java/com/example/imc/MainActivity.java
+++ app/src/main/java/com/example/imc/MainActivity.java
@@ -3,10 +3,10 @@ package com.example.imc;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -15,6 +15,10 @@ import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    TextView textoResultado;
+    ImageView imagenAsociada;
+    Double imc = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +32,9 @@ public class MainActivity extends AppCompatActivity {
         }else{
             Log.d("IMC", "Hay cosas guardas");
             //Coger las cosas guardadas
-            String textoGuardado = savedInstanceState.getString("resultado");
-            TextView textoCabecera = findViewById(R.id.textCabecera);
-            TextView textoResultado = findViewById(R.id.textResultado);
-            textoResultado.setText(textoGuardado);
-            textoCabecera.setText(getResources().getString(R.string.result));
+            Double imcGuadado = savedInstanceState.getDouble("imc");
+            imc = imcGuadado;
+            asignarResultados(imcGuadado);
         }
     }
 
@@ -40,12 +42,6 @@ public class MainActivity extends AppCompatActivity {
 
         TextView editEstatura = (TextView) findViewById(R.id.editEstatura);
         TextView editPeso = (TextView) findViewById(R.id.editPeso);
-
-        TextView cabecera = (TextView) findViewById(R.id.textCabecera);
-        TextView resultado = (TextView) findViewById(R.id.textResultado);
-
-        //setear imagen
-        ImageView imagen_asociada = findViewById(R.id.imageAsociada);
 
         String estatura = editEstatura.getText().toString();
         String peso = editPeso.getText().toString();
@@ -56,50 +52,55 @@ public class MainActivity extends AppCompatActivity {
         Double estaturaNum = Double.parseDouble(estatura);
         Double pesoNum = Double.parseDouble(peso);
 
-        Double imc = (pesoNum/(estaturaNum*estaturaNum));
-        String descripcion = "";
+        imc = (pesoNum/(estaturaNum*estaturaNum));
 
+        asignarResultados(imc);
+    }
+
+    private void asignarResultados(Double imc){
+
+        TextView cabecera = (TextView) findViewById(R.id.textCabecera);
+        TextView resultado = (TextView) findViewById(R.id.textResultado);
+        imagenAsociada = findViewById(R.id.imageAsociada);
+        String descripcion = "";
         if(imc > 0 ){
             cabecera.setText(getResources().getString(R.string.result));
             if(16 > imc ){
                 Log.d("IMC", "DESNUTRIDO");
                 descripcion = getResources().getString(R.string.undernourished);
-                imagen_asociada.setImageResource(R.drawable.girl);
+                imagenAsociada.setImageResource(R.drawable.girl);
             } else if(16 <= imc && imc <= 18 ){
                 Log.d("IMC", "DELGADO");
                 descripcion = getResources().getString(R.string.thin);
-                imagen_asociada.setImageResource(R.drawable.pantera_rosa);
+                imagenAsociada.setImageResource(R.drawable.pantera_rosa);
             } else if(18 <= imc && imc <= 25 ){
                 Log.d("IMC", "NORMAL");
                 descripcion = getResources().getString(R.string.normal);
-                imagen_asociada.setImageResource(R.drawable.normal);
+                imagenAsociada.setImageResource(R.drawable.normal);
             } else if(25 <= imc && imc <= 31 ){
                 Log.d("IMC", "SOBREPESO");
                 descripcion = getResources().getString(R.string.overweight);
-                imagen_asociada.setImageResource(R.drawable.sobrepeso);
+                imagenAsociada.setImageResource(R.drawable.sobrepeso);
             } else if(31 <= imc ) {
                 Log.d("IMC", "OBESO");
                 descripcion = getResources().getString(R.string.obese);
-                imagen_asociada.setImageResource(R.drawable.obeso);
+                imagenAsociada.setImageResource(R.drawable.obeso);
             }
 
             NumberFormat formatter = new DecimalFormat("#0.00");
 
             resultado.setText(getResources().getString(R.string.response) + " " + formatter.format(imc) + " - " + descripcion);
         }
-
     }
 
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
 
-        //1. Obtener el componente
-        TextView textoResultado = findViewById(R.id.textResultado);
-        //2. Recuperar el valor
-        String resultado = textoResultado.getText().toString();
-        //3 Guardar en el BUNDLE
-        outState.putString("resultado", resultado);
+        Log.d("IMC", "EN onSaveInstanceState");
+        if( imc != null){
+            outState.putDouble("imc", imc);
+        }
     }
 
     public void limpiarCampos(View view) {
@@ -117,6 +118,6 @@ public class MainActivity extends AppCompatActivity {
         cabecera.setText("");
         resultado.setText("");
         imagen_asociada.setImageResource(0);
-
+        imc = null;
     }
 }
