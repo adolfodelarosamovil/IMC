2f265de1b9b7b882b4e4401a6f802c27c88ff975
Adolfo <adolfodelarosa2012@gmail.com>
2019-10-07 19:25:37 +0200

    Opción de menú info que nos lleva a Wikipedia - IMC

--- app/src/main/java/com/example/imc/MainActivity.java
+++ app/src/main/java/com/example/imc/MainActivity.java
@@ -3,9 +3,14 @@ package com.example.imc;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -19,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     TextView textoResultado;
     ImageView imagenAsociada;
     Double imc = null;
+    private final String URL_IMC = "https://es.wikipedia.org/wiki/%C3%8Dndice_de_masa_corporal";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -119,5 +125,35 @@ public class MainActivity extends AppCompatActivity {
         resultado.setText("");
         imagen_asociada.setImageResource(0);
         imc = null;
+    }
+
+    //Debo sobrrescribir este método para cargar mi menú
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater mi = getMenuInflater();//este objeto será el encargado de cargar/inlfar mi menú
+        mi.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    //ESTE MÉTODO SE INVOCA AL TOCAR UNA OPCIÓN DEL MENÚ
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        Log.d("MIAPP", "Se ha tocado un elemento de la barra/menú");
+
+        switch (item.getItemId())
+        {
+            case R.id.info:
+                Log.d("MIAPP", "Ha tocado info");
+                //Intent que abre enlace URL
+                Intent intent = new Intent();
+                intent.setData(Uri.parse(URL_IMC));
+                intent.setAction(Intent.ACTION_VIEW);
+                if(intent.resolveActivity(getPackageManager()) != null){
+                    startActivity(intent);
+                }
+                break;
+        }
+
+        return true;//super.onOptionsItemSelected(item);
     }
 }
